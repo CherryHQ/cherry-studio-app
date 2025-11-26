e1003f43d7909f3441cdba8c4a80b1430297f321
eeee0717 <chentao020717Work@outlook.com>
2025-11-26 21:24:02 +0800

    chore: assistant selection style

--- src/componentsV2/features/ChatScreen/Header/AssistantSelection.tsx
+++ src/componentsV2/features/ChatScreen/Header/AssistantSelection.tsx
@@ -6,6 +6,7 @@ import { Keyboard, Pressable } from 'react-native'
 
 import { Text, XStack, YStack } from '@/componentsV2'
 import AssistantItemSheet from '@/componentsV2/features/Assistant/AssistantItemSheet'
+import { ChevronRight } from '@/componentsV2/icons'
 import type { Assistant, Topic } from '@/types/assistant'
 import type { DrawerNavigationProps } from '@/types/naviagate'
 
@@ -41,11 +42,14 @@ export const AssistantSelection: React.FC<AssistantSelectionProps> = ({ assistan
   return (
     <>
       <Pressable onPress={handlePress} className="active:opacity-60">
-        <XStack className="items-center justify-center gap-3.5">
+        <XStack className="items-center justify-center">
           <YStack className="items-center justify-start gap-0.5">
-            <Text className="text-text-primary text-base" ellipsizeMode="tail" numberOfLines={1}>
-              {assistant.name}
-            </Text>
+            <XStack className="items-center justify-start gap-0.5">
+              <Text className="text-text-primary text-base" ellipsizeMode="tail" numberOfLines={1}>
+                {assistant.name}
+              </Text>
+              <ChevronRight className="text-text-secondary" size={20} />
+            </XStack>
             <Text className="text-gray-60 text-[11px]" ellipsizeMode="tail" numberOfLines={1}>
               {topic.name}
             </Text>
