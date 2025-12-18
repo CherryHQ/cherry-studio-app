4b0b3981c2cce3be0b21d4d56656ec293cf491b1
eeee0717 <chentao020717Work@outlook.com>
2025-12-18 19:10:56 +0800

    fix: #237

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -14,6 +14,7 @@ import m0010 from './0010_sparkling_psynapse.sql';
 import m0011 from './0011_careful_newton_destine.sql';
 import m0012 from './0012_jittery_doctor_spectrum.sql';
 import m0013 from './0013_lush_madame_web.sql';
+import m0014 from './0014_gray_leopardon.sql';
 import journal from './meta/_journal.json';
 
   export default {
@@ -32,7 +33,8 @@ m0009,
 m0010,
 m0011,
 m0012,
-m0013
+m0013,
+m0014
     }
   }
   
