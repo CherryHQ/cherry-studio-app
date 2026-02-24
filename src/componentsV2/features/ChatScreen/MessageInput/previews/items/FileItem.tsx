80ca6887e52d41d98e3e2e6cc7ce38b1b4ed7c65
城城 <hello_world0000@outlook.com>
2026-02-24 13:10:56 +0800

    feat: removing icons and file extension

--- src/componentsV2/features/ChatScreen/MessageInput/previews/items/FileItem.tsx
+++ src/componentsV2/features/ChatScreen/MessageInput/previews/items/FileItem.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { View } from 'react-native'
 
 import Text from '@/componentsV2/base/Text'
-import { CodeIcon, PdfIcon, TableIcon, TxtIcon } from '@/componentsV2/icons'
+import { FileText } from '@/componentsV2/icons'
 import { loggerService } from '@/services/LoggerService'
 import type { FileMetadata } from '@/types/file'
 import { formatFileSize } from '@/utils/file'
@@ -27,26 +27,6 @@ const FileItem: FC<FileItemProps> = ({ file, onRemove, size, disabledContextMenu
     })
   }
 
-  // 处理文件名和扩展名
-  const fileNameParts = file.name.split('.')
-  const fileName = fileNameParts.slice(0, -1).join('.') || file.name
-  const fileExtension = fileNameParts.length > 1 ? fileNameParts[fileNameParts.length - 1]?.toLocaleUpperCase() : ''
-  const fileTypeMap = {
-    table: ['CSV', 'XLSX', 'XLS', 'NUMBERS'],
-    pdf: ['PDF'],
-    code: ['PY', 'TSX', 'JSON', 'CPP', 'C', 'HTML', 'CSS', 'JS', 'TS']
-  }
-  const renderFileIcon = () => {
-    if (fileTypeMap.table.includes(fileExtension)) {
-      return <TableIcon size={32} className="text-green-500" />
-    } else if (fileTypeMap.pdf.includes(fileExtension)) {
-      return <PdfIcon size={32} className="text-red-500" />
-    } else if (fileTypeMap.code.includes(fileExtension)) {
-      return <CodeIcon size={32} />
-    } else {
-      return <TxtIcon size={32} className="text-blue-500" />
-    }
-  }
   return (
     <BaseItem
       file={file}
@@ -59,15 +39,12 @@ const FileItem: FC<FileItemProps> = ({ file, onRemove, size, disabledContextMenu
           className="items-center justify-center rounded-2xl bg-zinc-400/20"
           style={{ width: height * 2.5, height: height }}>
           <View className="h-full w-full flex-row items-center gap-3 p-3">
-            {renderFileIcon()}
+            <FileText />
             <View className="flex-1">
               <Text className="w-full text-start text-base font-medium" numberOfLines={1} ellipsizeMode="middle">
-                {fileName}
+                {file.name}
               </Text>
-              <Text className="text-foreground-secondary text-sm">
-                {formatFileSize(file.size)}
-                {fileExtension && ` • ${fileExtension}`}
-              </Text>
+              <Text className="text-foreground-secondary text-sm">{formatFileSize(file.size)}</Text>
             </View>
           </View>
         </View>
